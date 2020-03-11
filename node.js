ddc19bdd6c457b76b13602932fb2c97521bebbf9
Queen Zcosha <kngarc@gmail.com>
2020-03-11 01:09:11 -0500

    blah blah blah

--- node.js
+++ node.js
@@ -4,7 +4,7 @@ var module3 = require('querystring');
 var module4 = require('path');
 var module5 = require('fs');
 var module6 = require('util');
-
+//--------------------------------------------------------------------------------------
 var detail = {
     name: function (name) {
       console.log('Name: ' + name);
@@ -21,10 +21,11 @@ var myLogModule = require('./Local_module.js');
 myLogModule.name('Edureka');
 myLogModule.domain('Education');
 
-
+//--------------------------------------------------------------------------------------
 
 var varName = value;
 
+//--------------------------------------------------------------------------------------
 
 //Defining a function
 function display_Name(firstName, lastName) {
@@ -33,5 +34,35 @@ function display_Name(firstName, lastName) {
    
 //Invoking the function
 display_Name("Kiki", "Garcia");
+
+//--------------------------------------------------------------------------------------
+
+// object with properties and method
+var employee = {
+    //properties
+    firstName: "Minho",
+    lastName: "Choi",
+    age: 35,
+    salary:50000,
+    //method
+    getFullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  };
+
+//--------------------------------------------------------------------------------------
+
+//ALREADY REQUIRED
+// var http = require('http');
+// var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('script.txt', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
+
+//--------------------------------------------------------------------------------------
 
 
