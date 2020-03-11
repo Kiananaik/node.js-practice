54a0ecda54dfda3a74da95860c8e39aa977f849e
Queen Zcosha <kngarc@gmail.com>
2020-03-11 00:36:13 -0500

    working through setup

---
+++ node.js
@@ -0,0 +1,26 @@
+var module1 = require('http');
+var module2 = require('url');
+var module3 = require('querystring');
+var module4 = require('path');
+var module5 = require('fs');
+var module6 = require('util');
+
+var detail = {
+    name: function (name) {
+      console.log('Name: ' + name);
+    },
+    domain:function (domain) {
+      console.log('Domain: ' + domain);
+    }
+};
+   
+module.exports = detail;
+
+var myLogModule = require('./Local_module.js');
+
+myLogModule.name('Edureka');
+myLogModule.domain('Education');
+
+
+
+var varName = value;
